3bf6831ee50c5a2f5f430a4cd8636ed100561796
Sage-v2 <122218278+Sage-v2@users.noreply.github.com>
2023-01-10 23:38:17 +0800

    Asta2.py

--- Asta2.py
+++ Asta2.py
@@ -392,7 +392,10 @@ def mbfv2():
                    peak()                  
                    print('\033[96mSemua ID\x1b[1;91m :\033[96m ' + str(len(id)) + "\n\033[92m \n",end="")
                    print('\n\033[95m [\033[1;92m+\033[95m] \033[96mhasil\033[92m Live\033[93m disimpan di \033[91m: \033[92mLive.txt\n \033[92m[\033[93m-\033[92m] \033[96mhasil\x1b[1;93m Ndrex\033[92m disimpan di \033[91m: \033[92mcp.txt')       
-                   def mengetik(s):
+                   import random
+import sys
+import time
+def mengetik(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
@@ -402,7 +405,6 @@ def mbfv2():
 #angka terkecil adalah yang paling cepat
 #angka terbesar adalah yang paling lambat
 mengetik('\n [\x1b[1;91m▪\x1b[1;92m] \033[92mMainkan Mode Pesawat 5 Detik Untuk Menjeda Proces Crek\n')
-                   detik()
                    print('\n [\x1b[1;97m▪\x1b[1;92m]\033[96m SEMOGA KAMU HOKY...! \n') 
                    with ThreadPoolExecutor(max_workers=30) as ex:
                           for user in username:
